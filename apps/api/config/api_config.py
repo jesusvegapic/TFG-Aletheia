5b9d34b354fb4ec8b3f29310f63cfc6169ac645e
jvega <jvega@ediversa.com>
2024-04-07 16:26:12 +0200

    parametrizadas variables de entorno y secrets para la configuración de la api rest.
    Desarrollado ci con github actions para automatizar la ejecución de los test.

--- apps/api/config/api_config.py
+++ apps/api/config/api_config.py
@@ -1,18 +1,24 @@
+import json
+import os
 from dataclasses import dataclass
 
 from apps.config.container import Config
 
+secrets_filename: str = os.getenv("SECRETS_FILENAME")  # type: ignore
 
-# env_filename = os.getenv("ENV_FILENAME", ".env")
+secrets_json: dict
+
+with open(secrets_filename, "r") as env_file:
+    secrets_json = json.loads(env_file.read())
+
+env_db = secrets_json["db"]
 
 
 @dataclass
 class ApiConfig(Config):
     APP_NAME: str = "Aletheia Courses platform API REST"
-    DEBUG: bool = True
-    DATABASE_ECHO: bool = True
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:password@postgres:5432/postgres"
+    DEBUG: bool = True if os.getenv("DEBUG") == 1 else 0  # type: ignore
+    DATABASE_ECHO: bool = True if os.getenv("DATABASE_ECHO") == 1 else 0  # type: ignore
+    DATABASE_URL: str = \
+        f"postgresql+asyncpg://{env_db['user']}:{env_db['passwd']}@{env_db['host']}:{env_db['port']}/{env_db['name']}"
     LOGGER_NAME: str = "api"
-
-# SECRET_KEY = config("SECRET_KEY", cast=Secret, default="secret")
-# ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default="*")
